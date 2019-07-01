40ab0a84317e34e4ce840ca8268a12c57148eb10
NomadJin <jinnykim000@gmail.com>
2019-07-01 14:05:54 +0900

    Prepared for deployment

--- sever.js
+++ sever.js
@@ -18,8 +18,9 @@ app.use("/api/profile", require("./routes/api/profile"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // set statuc folder
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
